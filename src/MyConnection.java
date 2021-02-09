fd33a41c842fc774956599a93e824bf6d8b2ba94
ashish2117 <sinhaashish2117@gmail.com>
2021-02-09 14:34:22 +0530

    Initial commit

---
+++ src/MyConnection.java
@@ -0,0 +1,34 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class MyConnection{
+   static Connection con=null;
+   public static Connection  createCon()
+   {
+       try
+       {
+       Class.forName("com.mysql.jdbc.Driver");
+       con=DriverManager.getConnection("jdbc:mysql://localhost:3306/uoh","root","ashish2117");
+       }
+       catch(Exception e)
+       {
+           JOptionPane.showMessageDialog(null, e.getMessage());
+       }
+       return con;
+   }
+}
